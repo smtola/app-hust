0bae5979a791572562c1b4beec7d22db2043204d
somtola <somtola.smt@gmail.com>
2023-10-19 11:45:16 +0700

    new update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  template: `
+  <div>
+    <router-outlet></router-outlet>
+  </div>
+`,
 })
 export class AppComponent implements OnInit {
   ngOnInit(): void {
